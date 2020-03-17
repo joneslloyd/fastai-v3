c7924c15b200171cd4b87afa3b01a62996bec08d
Lloyd Jones <joneslloyd@users.noreply.github.com>
2020-03-17 18:15:42 +0000

    Update for FastAI v2 and model, etc

--- app/server.py
+++ app/server.py
@@ -1,18 +1,19 @@
 import aiohttp
 import asyncio
 import uvicorn
-from fastai import *
-from fastai.vision import *
+from fastai2.data.all import *
+from fastai2.vision.all import *
+from fastai2.callback.all import *
 from io import BytesIO
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1FINdNAFu8sbLsBZN2O-M_s-dbXKKKvmD?alt=media&key=AIzaSyDkRQtcl8ER705GbCdDV_LfN5JD26xxSHI'
+export_file_name = 'stage-2a.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['none', 'other-ufo', 'feed', 'biofouling-plus-pink', 'biofouling', 'lice-males', 'lice-females', 'lice-preadult', 'salmon-scales']
 path = Path(__file__).parent
 
 app = Starlette()
@@ -59,7 +60,7 @@ async def homepage(request):
 async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
+    img = PILImage.create(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
